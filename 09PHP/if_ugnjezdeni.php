dc5d3fdd36a6ef7825d0fbb9f6efae47f48eb804
tomicevamarijana <tomiceva.marijana@gmail.com>
2023-04-24 22:55:06 +0200

    while 1

--- 09PHP/if_ugnjezdeni.php
+++ 09PHP/if_ugnjezdeni.php
@@ -93,73 +93,7 @@
         echo"<p>$n je paran.</p>";
     }
 
-    //ZADATAK 21 
-    echo"<hr><p>Zadatak 21</p>";
-    echo"<p>Naći koji je najveći od tri uneta broja a, b i c.</p>";
-    $a=25;
-    $b=100;
-    $c=20;
-    $max=$a;
-    if($b>$max){
-        $max=$b;
-    }
-    if($c>$max){
-        $max=$c;
-    }
-    echo"<p>Najveci broj od unetih $a, $b i $c je $max.</p>";
     
-    //ZADATAK 22 
-    echo"<hr><p>Zadatak 22</p>";
-    echo"<p>Ispisati na ekranu „ekstremna temperatura“ ukoliko je temperatura manja od -15 stepeni Celzijusovih  i veća od 40 stepeni Celzijusovih.</p>";
-    $t=-16;
-    if($t<-15 || $t>40){
-        echo"<p>Temperatura $t je EKSTREMNA.</p>";
-    }
-
-    //ZADATAK 23 
-    echo"<hr><p>Zadatak 23</p>";
-    echo"<p>Ispitati da li je godina prestupna. (Godinu preuzeti sa vremena na Vašem računaru).
-    Prestupna je ona godina koja je deljiva sa 4, ako nije deljiva sa 100, mada ipak jeste deljiva sa 400.(сем у случају да је дељива са 100 а није са 400)
-    </p>";
-    //$y=date('Y');
-    $y=1800;
-    if($y%400==0){
-        echo"<p>Godina $y je prestupna.</p>";
-    }
-    elseif($y%100!=0 && $y%4==0){
-        echo"<p>Godina $y je prestupna.</p>";
-    }
-    else{
-        echo"<p>Godina $y je prosta.</p>";
-    }
-
-    //ZADATAK 24
-    echo"<hr><p>Zadatak 24</p>";
-    echo"<p>Za uneto početno i krajnje radno vreme dva lekara, ispitati koliko sati i minuta se njihove smene preklapaju.</p>";
-    $p1=12;
-    $k1=16;
-    $p2=8;
-    $k2=18;
-    if($k1<$p2 || $k2<$p1){
-        echo"<p>Smene se ne preklapaju.</p>";
-    }
-    elseif($p1<$p2 && $k1<$k2){
-        $p=$k1-$p2;
-        echo"<p>Smene se preklapaju za $p sati.</p>";
-    }
-    elseif($p2<$p1 && $k2<$k1){
-        $p=$k2-$p1;
-        echo"<p>Smene se preklapaju za $p sati.</p>";
-    }
-    elseif($p1>$p2 && $k1<$k2){
-        $p=$k1-$p1;
-        echo"<p>Smene se preklapaju za $p sati.</p>";
-    }
-    elseif($p1<$p2 && $k1>$k2){
-        $p=$k2-$p2;
-        echo"<p>Smene se preklapaju za $p sati.</p>";
-    }
-
 
 
 
---
+++ 11WHILE/index_while.php
@@ -0,0 +1,271 @@
+<?php
+//While petlja
+/*
+    1.Postavite pocetnu vrednost-inicijalizacija
+    2.while(uslov)
+        2.a.TRUE-Blok naredbi ukoliko je uslov u petlji ispunjen
+        2.b.FALSE-Ne izvrsava se blok naredbi unutar while petlje, vec se prelazi na kod ispod
+ */
+
+ //1.zadatak
+ //Ispisati brojeve od 1-20:(ukljuci i 20)
+ //a.sve u istom redu
+ //b.Svaki u novom redu
+    echo"<p>Zadatak 1</p>";
+
+    echo"<p>Zadatak 1. b</p>";
+    $i=1;
+    while($i<=20){
+        echo"$i ";
+        $i++; //bez ovog bi petlja bila beskonacna
+    }
+    echo"<i>$i</i>";//vrednost $i nakon petlje je 21;
+
+    echo"<p>Zadatak 1. b</p>";
+
+    $i=1;
+    while($i<=20){
+        echo"$i <br>";
+        $i++; 
+    }
+    echo"<i>$i</i>";
+    //Ovo su ekvivalentni izrazi za povecavanje za 1:
+    //$i++; 
+    //$i+=1;
+    //$i=$i+1;
+
+    //Ovo su ekvivalentni izrazi za smanjivanje za 1:
+    //$i--l
+    //$i-=1;
+    //$i-1;
+
+    echo"<p>Zadatak 2</p>";
+    $i=20;
+    while($i>=1){
+        echo"$i ";
+        $i--;
+    }
+    echo"<i>$i</i>"; //nakon izvrsenja petlje $i je 0;
+    
+    //Zadatak 4.
+    echo"<p>Zadatak 4</p>";
+    echo"<p>Kreirati n paragrafa sa proizvoljnim tekstom 
+    i naizmenično ih obojiti u tri različite boje</p>";
+    echo"<p>Zadatak 4-PRVI NACIN</p>";
+    $n=5;
+    $i=1;
+    while($i<=$n){
+        if($i%3==0){
+            echo"<p style='color:red;'>Hello $i</p>";
+        }
+        elseif($i%3==1){
+            echo"<p style='color:blue;'>Hello $i</p>";
+        }
+        else{
+            echo"<p style='color:orange;'>Hello $i</p>";
+
+        }       
+        $i++;
+    }
+
+    echo"<p>Zadatak 4-DRUGI NACIN</p>";
+    $n=5;
+    $i=1;
+    while($i<=$n){
+        if($i%3==0){
+            $boja="purple";
+        }
+        elseif($i%3==1){
+            $boja="lime";
+        }
+        else{
+            $boja="magenta";
+        }
+        echo"<p style='color:$boja;'>Hello $i</p>";       
+        $i++;
+    }
+
+    echo"<p>Zadatak 4-TRECI NACIN</p>";
+    $n=5;
+    $i=1;
+    while($i<=$n){
+        $boja="magenta";
+        if($i%3==0){
+            $boja="purple";
+        }
+        elseif($i%3==1){
+            $boja="lime";
+        }        
+        echo"<p style='color:$boja;'>Hello $i</p>";       
+        $i++;
+    }
+
+    echo"<p>Zadatak 3</p>";
+    echo"<p>Ispisati parne brojeve od 1 do 20.</p>";
+    $i=1;
+    while($i<=20){
+        if($i%2==0){
+            echo"$i, ";
+        }
+        $i++;
+    }
+    //moglo je sa brojacom $i=$i+2; bez if-a
+
+    echo"<p>Zadatak 5</p>";
+    echo"<p>Kreirati n proizvoljnih slika i staviti im naizmenično dva različita okvira</p>";
+    $n=4;
+    $i=1;
+    
+    while($i<=$n){
+        if($i%2==0){
+            echo "<p><img src='maslacak.jpg' style='border:5px solid red;'></p>";
+        }
+        else{
+            echo "<p><img src='maslacak.jpg' style='border:5px solid blue;'></p>";
+        }
+        $i++;
+    }
+
+    //Drugi nacin-ne svidja mi se
+    /*
+    echo"<p>Zadatak 5-Drugi nacin</p>";
+    $n=4;
+    $i=1;
+    $red=" 'border:5px solid red';";
+    $blue="'border:5px solid red';";
+    while($i<=$n){        
+        if($i%2==0){
+            echo "<p><img src='maslacak.jpg' $red></p>";
+        }
+        else{
+            echo "<p><img src='maslacak.jpg' style='.$red. ></p>";
+        }
+        $i++;
+    }
+    */
+
+    echo"<p>Zadatak X</p>";
+    echo"<p>ispisi n paragrafa svaki u random boji</p>";
+    $n=10;
+    $i=1;    
+    while($i<=$n){
+        $r=rand(0,255);
+        $g=rand(0,255);
+        $b=rand(0,255);
+        $boja="rgb({$r},{$g},{$b})";                
+        echo"<p style='color:$boja;'>Paragraf broj $i</p>";        
+        $i++;
+    }
+
+    //6.zadatak
+    echo"<p>Zadatak X</p>";
+    echo"<p>Odrediti sumu brojeva od 1 do 100.</p>";
+    $i=1;
+    $s=0;
+    while($i<=100){
+        $s=$s+$i; //$s+=$i;
+        $i++;
+    }
+    echo"<p>Suma brojeva od 1 do 100 je: $s</p>";
+
+    //7 zadatak
+    echo"<p>Zadatak 7</p>";
+    echo"<p>Odrediti sumu brojeva od 1 do n.</p>";
+    
+    $i=1;
+    $n=100;
+    $s=0;
+    while($i<=$n){
+        $s=$s+$i;
+        $i++;
+    }
+    echo"<p>Suma brojeva od 1 do $n je: $s</p>";
+
+    //8 zadatak
+    echo"<p>Zadatak 8</p>";
+    echo"<p>Odrediti sumu brojeva od n do m</p>";
+
+    $n=2;
+    $m=100;
+    $s=0;
+    $pom=$n; //cuva staru pocetnu vrednost n
+    //$n=$i=10 moze ovako
+
+    while($n<=$m){
+        $s=$s+$n;
+        
+        //echo "<p>$n</p>";
+        //echo "<p>$s</p>";
+        $n++;
+    }
+    echo"<p>Suma brojeva od $pom do $m je: $s</p>";
+
+    //9 zadatak
+    echo"<p>Zadatak 9</p>";
+    echo"<p>Odrediti proizvod brojeva od n do m.</p>";
+    $n=$i=5;
+    $m=7;
+    $p=1;
+    while($i<=$m){
+        $p=$p*$i;
+        $i++;
+    }
+    echo"<p>Proizvod brojeva od $n do $m je: $p</p>";
+
+    //10 zadatak
+    echo"<p>Zadatak 10</p>";
+    echo"<p>Odrediti sumu kvadrata parnih i sumu kubova neparnih brojeva od n do m</p>";
+    $n=$i=1;
+    $m=4;
+    $sP=0;
+    $sN=0;
+    while($i<$m){
+        if($i%2==0){
+            $sP=$sP+$i*$i; //$i**2
+        }
+        else{
+            $sN=$sN+$i*$i*$i; //$i**3
+        }
+        $i++;
+    }
+    echo"<p>Suma kvadrata parnih brojeva od $n do $m je: $sP</p>";
+    echo"<p>Suma kubova neparnih brojeva od $n do $m je: $sN</p>";
+
+    //11 zadatak
+    echo"<p>Zadatak 11</p>";
+    echo"<p>Odrediti sa koliko brojeva je deljiv uneti broj k.</p>";
+    $i=1;
+    $k=10;
+    $br=0;
+    while($i<=$k){
+        if($k%$i==0)
+        $br=$br+1;
+        $i++;
+    }
+    echo"<p>Broj $k deljiv je sa $br broja</p>";
+    
+
+    
+
+
+
+    
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+ 
+
+?>
