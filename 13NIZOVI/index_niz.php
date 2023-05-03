9c693775ddb652aecdd3eb25aea3009d12326095
tomicevamarijana <tomiceva.marijana@gmail.com>
2023-05-03 22:05:39 +0200

    nizovi_vezbanje_5.3.23 cas

--- 13NIZOVI/index_niz.php
+++ 13NIZOVI/index_niz.php
@@ -225,6 +225,241 @@
         echo "$sviIndeksiMaks[$i], "; 
     }
     //implode() za ispisivanje niza kao string
+    
+
+    //nastavak - 5.3.2023.
+    echo"<p><hr></p>"; 
+
+    //zadatak 7
+    echo"<p><b>ZADATAK 7</b></p>";    
+    echo"<p>Odrediti broj elemenata celobrojnog niza koji su veći od srednje vrednosti. *</p>";
+
+    $brojevi=[5,6,14,-4,14,11,-7,14];
+    $br=0;
+    $s=0;
+    for($i=0;$i<count($brojevi);$i++){
+        $s=$s+$brojevi[$i];
+        
+    }    
+    $sr=$s/count($brojevi);
+    for($i=0;$i<count($brojevi);$i++){
+        if($brojevi[$i]>$sr){
+            $br=$br+1; //$br++
+        }
+    }
+    echo"<p>Broj elemenata celobrojnog niza koji su veći od srednje vrednosti koja iznosi $sr je $br .</p>";
+
+    //zadatak 8
+    echo"<p><b>ZADATAK 8</b></p>";    
+    echo"<p>Izračunati zbir pozitivnih elemenata celobrojnog niza.</p>";
+
+    $s=0;
+    $brojevi=[5,6,14,-4,14,11,-7,14];
+    for($i=0;$i<count($brojevi);$i++){
+        if($brojevi[$i]>0){
+            $s=$s+$brojevi[$i];
+        }
+    }
+    echo"<p>Zbir pozitivnih elemenata celobrojnog niza je $s.</p>";
+
+    //BONUS zadatak 
+    echo"<p><b>BONUS ZADATAK </b></p>";    
+    echo"<p>Izračunati srednju vrednost parnih elemenata celobr niza.</p>";
+
+    $br=0;
+    $s=0;
+    $brojevi=[2,2,2,2];
+    for($i=0;$i<count($brojevi);$i++){
+        if($brojevi[$i]%2==0){
+            $br++;
+            $s=$s+$brojevi[$i];
+        }
+    }
+    if($br!=0){
+        $srvr=$s/$br; //obratiti paznju na deljenje nulom!
+    }
+    else{
+        $srvr=0; //nema parnih elemenata
+    }
+    echo"<p>Srednja vrednost parnih elemenata celobr niza je $srvr.</p>";
+    
+
+
+
+    //zadatak 9
+    echo"<p><b>ZADATAK 9</b></p>";    
+    echo"<p>Odrediti broj parnih elemenata u celobrojnom nizu. *</p>";
+
+    $brojevi=[5,6,14,-4,14,11,-7,14];    
+    $br=0;
+    for($i=0;$i<count($brojevi);$i++){
+        if($brojevi[$i]%2==0){
+            $br=$br+1;
+        }
+    }
+    echo"<p>Broj parnih elemenata u celobrojnom nizu je $br.</p>";
+
+    //zadatak 10
+    echo"<p><b>ZADATAK 10</b></p>";    
+    echo"<p> Izračunati sumu elemenata u nizu sa parnim indeksom. </p>";
+    //$brojevi=[5,6,14,-4,14,11,-7,14];
+    $brojevi=[2,2,2,2];
+    $s=0;
+    for($i=0;$i<count($brojevi);$i++){
+        if($i%2==0){
+            $s=$s+$brojevi[$i];
+        }
+    }
+    echo"<p>Suma elemenata u nizu sa parnim indeksom je $s.</p>";
+    //ovde je dobro koristiti korak $i+=2, kada se traze parni indeksi
+
+    //zadatak 11
+    echo"<p><b>ZADATAK 11</b></p>";    
+    echo"<p>Promeniti znak svakom elementu celobrojnog niza.</p>";
+    $brojevi=[2,2,2,2];
+    for($i=0;$i<count($brojevi);$i++){
+        $brojevi[$i]=-$brojevi[$i];
+    }
+    //ispis niza
+    for($i=0;$i<count($brojevi);$i++){
+        echo $brojevi[$i] . ", ";
+    }
+
+    //zadatak 12
+    echo"<p><b>ZADATAK 12</b></p>";    
+    echo"<p>Promeniti znak svakom neparnom elementu celobrojnog niza sa parnim indeksom. *</p>";
+    $brojevi=[2,2,3,3,4,4,5,5];
+    for($i=0;$i<count($brojevi);$i++){
+        if($brojevi[$i]%2!=0 && $i%2==0){
+            $brojevi[$i]=-$brojevi[$i];
+        }
+    }
+    //ispis niza
+    for($i=0;$i<count($brojevi);$i++){
+        echo $brojevi[$i] . ", ";
+    }
+
+    //zadatak 12-bolji nacin
+    echo"<p><b>ZADATAK 12-BOLJI NACIN</b></p>";
+
+    $brojevi=[2,2,3,3,4,4,5,5];
+    for($i=0;$i<count($brojevi);$i+=2){
+        if($brojevi[$i]%2!=0){
+            $brojevi[$i]=-$brojevi[$i];
+        }        
+    }
+    //ispis niza
+    for($i=0;$i<count($brojevi);$i++){
+        echo $brojevi[$i] . ", ";
+    }
+
+    //zadatak 13
+    echo"<p><b>ZADATAK 13 -proba</b></p>";    
+    echo"<p>Odrediti broj parnih elemenata sa neparnim indeksom. *</p>";
+    $brojevi=[2,2,3,3,4,4,5,5];
+    $br=0;
+    for($i=1;$i<=count($brojevi);$i+=2){
+        if($brojevi[$i]%2==0){
+            $br=$br+1;
+        }
+    }
+    echo"<p>Broj parnih elemenata sa neparnim indeksom je $br -PROBA.</p>";
+
+    //zadatak 13
+    echo"<p><b>ZADATAK 13-normalno</b></p>";
+
+    $brojevi=[2,2,3,3,4,4,5,5,6];
+    $br=0;
+    for($i=0;$i<count($brojevi);$i++){
+        if($brojevi[$i]%2==0 && $i%2!=0){
+            $br=$br+1;
+        }
+    }
+    echo"<p>Broj parnih elemenata sa neparnim indeksom je $br.</p>";
+
+    //zadatak 14
+    echo"<p><b>ZADATAK 14</b></p>";    
+    echo"<p>Ispisati dužinu svakog elementa u nizu stringova.</p>";
+
+    $imena=["Stefan","Aleksandar","Adam","Marija","Dunja","Milijana"];
+    for($i=0;$i<count($imena);$i++){
+        $ime=$imena[$i];
+        //$duzina=strlen($ime);
+        $duzina=mb_strlen($ime,"UTF-8");//za srpsku latinicu
+        echo"<p>Dužinu stringa $ime je $duzina.</p>";
+    }
+
+    //zadatak 15
+    echo"<p><b>ZADATAK 15</b></p>";    
+    echo"<p>Odrediti element u nizu stringova sa najvećom dužinom.</p>";
+
+    $imena=["Stefan","Aleksandar","Adam","Marija","Dunja","Milijana"];
+    $maxDuzina=strlen($imena[0]);
+    $maxString=$imena[0];
+    for($i=1;$i<count($imena);$i++){ //ovde namerno preskacem $i=0 jer nema potrebe kad je vec zadat
+        if(strlen($imena[$i])>$maxDuzina){
+            $maxDuzina=strlen($imena[$i]);
+            $maxString=$imena[$i];
+        }
+    }
+    echo"<p>El. u nizu stringova sa najvećom dužinom je $maxString.</p>";
+
+    //zadatak 17
+    echo"<p><b>ZADATAK 17-substring-STRPOS</b></p>";    
+    echo"<p>Odrediti element u nizu stringova koji sadrzi substring, tj.podstring.</p>";
+    var_dump(strpos("Sreda","a")); //INDEXI u stringu krecu od 0; 
+    //Vraca se INDEX prvog pojavljivanja;
+    if(strpos("Sreda","S")!==false){
+        echo"<p>String 'S' se nalazi u stringu 'Sreda'</p>";
+    }
+    else{
+        echo"<p>String 'S' se ne nalazi u stringu 'Sreda'</p>";
+    }
+
+    //zadatak 17
+    echo"<p><b>ZADATAK 17</b></p>";    
+    echo"<p>Odrediti broj elemenata u nizu stringova koji sadrže slovo 'a'. *</p>";
+
+    $imena=["Stefan","Aleksandar","Adam","Marija","Dunja","Milijana"];
+    $br=0;
+    for($i=0;$i<count($imena);$i++){
+        if(strpos($imena[$i],'a')!==false){
+            $br++;
+        }
+    }
+    echo"<p>Broj elemenata u nizu stringova koji sadrže slovo 'a' je $br.</p>";
+
+    //zadatak 18
+    echo"<p><b>ZADATAK 18</b></p>";    
+    echo"<p>Odrediti broj elemenata u nizu stringova koji počinju na slovo 'a' ili 'A'.*</p>";
+
+    $imena=["Stefan","Aleksandar","Adam","Marija","Dunja","Milijana"];
+    $br=0;
+    for($i=0;$i<count($imena);$i++){
+        if(strpos($imena[$i],'a')===0 || strpos($imena[$i],'A')===0){
+            $br++;
+        }
+    }
+    echo"<p>Broj elemenata u nizu stringova koji pocinju na slovo 'a' ili 'A' je $br.</p>";
+
+
+
+
+
+
+
+
+    
+    
+
+
+
+
+
+
+
+
+
 
     
 
