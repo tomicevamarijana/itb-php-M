31639cc33220402abe4dc56aa3860372972d7984
tomicevamarijana <tomiceva.marijana@gmail.com>
2023-05-11 21:30:47 +0200

    projekat dan X-skoro gotov

--- PROJEKAT/index.php
+++ PROJEKAT/index.php
@@ -56,10 +56,81 @@
         </div>
     </header>
 
-    <!-- PHP main section -->
-    <?php        
+    <!-- novi nav -->
 
-    ?>
+    <div class="accordion" id="accordionExample">
+        <div class="accordion-item">
+            <h2 class="accordion-header" id="headingOne">
+                <button class="accordion-button" type="button" data-bs-toggle="collapse" data-bs-target="#collapseOne"
+                    aria-expanded="true" aria-controls="collapseOne">
+                    Accordion Item #1
+                </button>
+            </h2>
+            <div id="collapseOne" class="accordion-collapse collapse" aria-labelledby="headingOne"
+                data-bs-parent="#accordionExample">
+                <div class="accordion-body">
+                    <strong>This is the first item's accordion body.</strong> It is shown by default, until the collapse
+                    plugin adds the appropriate classes that we use to style each element. These classes control the
+                    overall appearance, as well as the showing and hiding via CSS transitions. You can modify any of
+                    this with custom CSS or overriding our default variables. It's also worth noting that just about any
+                    HTML can go within the <code>.accordion-body</code>, though the transition does limit overflow.
+                </div>
+            </div>
+        </div>
+        <div class="accordion-item">
+            <h2 class="accordion-header" id="headingTwo">
+                <button class="accordion-button collapsed" type="button" data-bs-toggle="collapse"
+                    data-bs-target="#collapseTwo" aria-expanded="false" aria-controls="collapseTwo">
+                    Accordion Item #2
+                </button>
+            </h2>
+            <div id="collapseTwo" class="accordion-collapse collapse" aria-labelledby="headingTwo"
+                data-bs-parent="#accordionExample">
+                <div class="accordion-body">
+                    <strong>This is the second item's accordion body.</strong> It is hidden by default, until the
+                    collapse plugin adds the appropriate classes that we use to style each element. These classes
+                    control the overall appearance, as well as the showing and hiding via CSS transitions. You can
+                    modify any of this with custom CSS or overriding our default variables. It's also worth noting that
+                    just about any HTML can go within the <code>.accordion-body</code>, though the transition does limit
+                    overflow.
+                </div>
+            </div>
+        </div>
+        <div class="accordion-item">
+            <h2 class="accordion-header" id="headingThree">
+                <button class="accordion-button collapsed" type="button" data-bs-toggle="collapse"
+                    data-bs-target="#collapseThree" aria-expanded="false" aria-controls="collapseThree">
+                    Accordion Item #3
+                </button>
+            </h2>
+            <div id="collapseThree" class="accordion-collapse collapse" aria-labelledby="headingThree"
+                data-bs-parent="#accordionExample">
+                <div class="accordion-body">
+                    <strong>This is the third item's accordion body.</strong> It is hidden by default, until the
+                    collapse plugin adds the appropriate classes that we use to style each element. These classes
+                    control the overall appearance, as well as the showing and hiding via CSS transitions. You can
+                    modify any of this with custom CSS or overriding our default variables. It's also worth noting that
+                    just about any HTML can go within the <code>.accordion-body</code>, though the transition does limit
+                    overflow.
+                </div>
+            </div>
+        </div>
+    </div>
+
+    
+
+      <!-- novi main -->
+   
+        
+          
+        
+        
+      
+
+    
+
+    <!-- stari nav -->
+    
     <nav class="navbar navbar-expand-sm bg-light">
         <div class="container-fluid">
             <ul class="navbar-nav">
@@ -78,6 +149,8 @@
             </ul>
         </div>
     </nav>
+
+
     <!-- -----------------------------------------PHP pocetak------------------------------------------>
 
     <!-- -----------------------------------------MOTIVACIJA---------------------------------------- -->
@@ -122,7 +195,7 @@
         }               
     }
     //echo $randAutorM;
-    //fclose($myfile);
+    fclose($myfileM);
 ?>
 <!-- -----------------------------------------POSAO---------------------------------------- -->
 <?php
@@ -150,7 +223,7 @@
         }               
     }
     //echo $randAutorP;
-    //fclose($myfile);
+    fclose($myfileP);
 ?>
 <!-- -----------------------------------------LJUBAV---------------------------------------- -->
 <?php
@@ -177,7 +250,7 @@
         }
     }
     //echo $randAutorLj;
-    //fclose($myfile);
+    fclose($myFileLj);
 ?>
 <!-- -----------------------------------------ZDRAVLJE---------------------------------------- -->
 <?php
@@ -204,6 +277,7 @@
         }
     }
     //echo $randAutorZ;
+    fclose($myFileZ);
 ?>
 <!-- -----------------------------------------4u1---------------------------------------- -->
 <?php
@@ -267,6 +341,10 @@
             </div>
         </div>
     </div>
+<!-- nov section  -->
+ 
+
+
 
     <!-- footer -->
     <footer>
