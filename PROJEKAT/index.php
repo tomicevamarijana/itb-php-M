e9cd0f52ab2700d0d31923c0ebab5b91cc9f5beb
tomicevamarijana <tomiceva.marijana@gmail.com>
2023-05-12 08:46:21 +0200

    final_deleted

--- PROJEKAT/index.php
+++ PROJEKAT/index.php
@@ -4,157 +4,23 @@
     <meta charset="UTF-8">
     <meta http-equiv="X-UA-Compatible" content="IE=edge">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
-    <title>Projekat PHP</title>
-    <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.2.3/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-rbsA2VBKQhggwzxH7pPCaAqO46MgnOM80zW1RWuH61DGLwZJEdK2Kadq2F9CUG65" crossorigin="anonymous">
+    <title>4 Projekat PHP</title>
+    
+    <link rel="stylesheet" href="https://cdn.jsdelivr.net/npm/bootstrap@4.6.2/dist/css/bootstrap.min.css" integrity="sha384-xOolHFLEh07PJGoPkLv1IbcEPTNtaed2xpHsD9ESMhqIYd0nLMwNLD69Npy4HI+N" crossorigin="anonymous">
     <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/4.7.0/css/font-awesome.min.css">
     <link rel="stylesheet" href="stil.css">
 </head>
 <body>
-    <?php
-        // ucitavanje slika za heder
+    <!-- -----------------------------------------PHP pocetak------------------------------------------>
+    <!--ucitavanje slika za heder-->
+    <?php        
         $arrayPh=[];
         for($i=1;$i<=3;$i++){            
             $arrayPh[]=rand(1,10);
         }
-    ?>
-
-    <header>
-        <div class="container">
-            <div class="row">
-                <!-- Carousel -->
-                <div id="demo" class="carousel slide" data-bs-ride="carousel">
-
-                    <!-- Indicators/dots -->
-                    <div class="carousel-indicators">
-                        <button type="button" data-bs-target="#demo" data-bs-slide-to="0" class="active"></button>
-                        <button type="button" data-bs-target="#demo" data-bs-slide-to="1"></button>
-                        <button type="button" data-bs-target="#demo" data-bs-slide-to="2"></button>
-                    </div>
-
-                    <!-- The slideshow/carousel -->
-                    <div class="carousel-inner">
-                        <div class="carousel-item active">
-                            <img src="images/<?php echo $arrayPh[0]?>.jpg" alt="<?php echo $arrayPh[0]?>" class="d-block" style="width:100%">
-                        </div>
-                        <div class="carousel-item">
-                            <img src="images/<?php echo $arrayPh[1]?>.jpg" alt="<?php echo $arrayPh[1]?>" class="d-block" style="width:100%">
-                        </div>
-                        <div class="carousel-item">
-                            <img src="images/<?php echo $arrayPh[2]?>.jpg" alt="<?php echo $arrayPh[2]?>" class="d-block" style="width:100%">
-                        </div>
-                    </div>
-
-                    <!-- Left and right controls/icons -->
-                    <button class="carousel-control-prev" type="button" data-bs-target="#demo" data-bs-slide="prev">
-                        <span class="carousel-control-prev-icon"></span>
-                    </button>
-                    <button class="carousel-control-next" type="button" data-bs-target="#demo" data-bs-slide="next">
-                        <span class="carousel-control-next-icon"></span>
-                    </button>
-                </div>
-            </div>
-        </div>
-    </header>
-
-    <!-- novi nav -->
-
-    <div class="accordion" id="accordionExample">
-        <div class="accordion-item">
-            <h2 class="accordion-header" id="headingOne">
-                <button class="accordion-button" type="button" data-bs-toggle="collapse" data-bs-target="#collapseOne"
-                    aria-expanded="true" aria-controls="collapseOne">
-                    Accordion Item #1
-                </button>
-            </h2>
-            <div id="collapseOne" class="accordion-collapse collapse" aria-labelledby="headingOne"
-                data-bs-parent="#accordionExample">
-                <div class="accordion-body">
-                    <strong>This is the first item's accordion body.</strong> It is shown by default, until the collapse
-                    plugin adds the appropriate classes that we use to style each element. These classes control the
-                    overall appearance, as well as the showing and hiding via CSS transitions. You can modify any of
-                    this with custom CSS or overriding our default variables. It's also worth noting that just about any
-                    HTML can go within the <code>.accordion-body</code>, though the transition does limit overflow.
-                </div>
-            </div>
-        </div>
-        <div class="accordion-item">
-            <h2 class="accordion-header" id="headingTwo">
-                <button class="accordion-button collapsed" type="button" data-bs-toggle="collapse"
-                    data-bs-target="#collapseTwo" aria-expanded="false" aria-controls="collapseTwo">
-                    Accordion Item #2
-                </button>
-            </h2>
-            <div id="collapseTwo" class="accordion-collapse collapse" aria-labelledby="headingTwo"
-                data-bs-parent="#accordionExample">
-                <div class="accordion-body">
-                    <strong>This is the second item's accordion body.</strong> It is hidden by default, until the
-                    collapse plugin adds the appropriate classes that we use to style each element. These classes
-                    control the overall appearance, as well as the showing and hiding via CSS transitions. You can
-                    modify any of this with custom CSS or overriding our default variables. It's also worth noting that
-                    just about any HTML can go within the <code>.accordion-body</code>, though the transition does limit
-                    overflow.
-                </div>
-            </div>
-        </div>
-        <div class="accordion-item">
-            <h2 class="accordion-header" id="headingThree">
-                <button class="accordion-button collapsed" type="button" data-bs-toggle="collapse"
-                    data-bs-target="#collapseThree" aria-expanded="false" aria-controls="collapseThree">
-                    Accordion Item #3
-                </button>
-            </h2>
-            <div id="collapseThree" class="accordion-collapse collapse" aria-labelledby="headingThree"
-                data-bs-parent="#accordionExample">
-                <div class="accordion-body">
-                    <strong>This is the third item's accordion body.</strong> It is hidden by default, until the
-                    collapse plugin adds the appropriate classes that we use to style each element. These classes
-                    control the overall appearance, as well as the showing and hiding via CSS transitions. You can
-                    modify any of this with custom CSS or overriding our default variables. It's also worth noting that
-                    just about any HTML can go within the <code>.accordion-body</code>, though the transition does limit
-                    overflow.
-                </div>
-            </div>
-        </div>
-    </div>
-
-    
-
-      <!-- novi main -->
-   
-        
-          
-        
-        
-      
-
-    
-
-    <!-- stari nav -->
-    
-    <nav class="navbar navbar-expand-sm bg-light">
-        <div class="container-fluid">
-            <ul class="navbar-nav">
-                <li class="nav-item">
-                    <a class="nav-link " class="btn btn-primary" data-bs-toggle="collapse" href="#collapseExampleP" role="button" aria-expanded="false" aria-controls="collapseExampleP">POSAO</a>
-                </li>
-                <li class="nav-item">
-                    <a class="nav-link" class="btn btn-primary" data-bs-toggle="collapse" href="#collapseExampleZ" role="button" aria-expanded="false" aria-controls="collapseExampleZ">ZDRAVLJE</a>
-                </li>
-                <li class="nav-item">
-                    <a class="nav-link" class="btn btn-primary" data-bs-toggle="collapse" href="#collapseExampleLj" role="button" aria-expanded="false" aria-controls="collapseExampleLj">LJUBAV</a>
-                </li>
-                <li class="nav-item">
-                    <a class="nav-link" class="btn btn-primary" data-bs-toggle="collapse" href="#collapseExampleM" role="button" aria-expanded="false" aria-controls="collapseExampleM">MOTIVACIJA</a>
-                </li>
-            </ul>
-        </div>
-    </nav>
-
-
-    <!-- -----------------------------------------PHP pocetak------------------------------------------>
+    ?>       
 
     <!-- -----------------------------------------MOTIVACIJA---------------------------------------- -->
-
 <?php
     //Logika se zasniva na sledecem:
     // 1. ucita se txt i od njega napravi niz $array*
@@ -214,7 +80,7 @@
         $randIndexP=rand(0,(count($citatiP)-1));
         $randCitatP=$citatiP[$randIndexP];              
     }
-    //echo $randCitatP; //obavezno VAN: da se u petlji ne bi kalemili jedan na drugi
+    //echo $randCitatP; 
 
     for($i=0;$i<(count($arrayP)-1);$i++){
         if($arrayP[$i]==$randCitatP){            
@@ -313,53 +179,147 @@
     //echo $randAutor;
 ?>
     <!-- -----------------------------------------PHP-kraj---------------------------------------- -->
-    
-    <!-- main section -->
-    <div class="container p-5 my-5 bg-dark text-white">
-        <div class="row">
-            <div class="col-12">
-                <div id="init">
-                    <cite><?php echo $randCitat;?></cite>
-                    <p><?php echo $randAutor;?></p>
+
+    <header>
+
+        <div id="demo" class="carousel slide" data-ride="carousel">
+
+            <!-- Indicators -->
+            <ul class="carousel-indicators">
+                <li data-target="#demo" data-slide-to="0" class="active"></li>
+                <li data-target="#demo" data-slide-to="1"></li>
+                <li data-target="#demo" data-slide-to="2"></li>
+            </ul>
+
+            <!-- The slideshow -->
+            <div class="carousel-inner">
+                <div class="carousel-item active">
+                    <img src="images/<?php echo $arrayPh[0]?>.jpg" alt="<?php echo $arrayPh[0]?>" >
                 </div>
-                <div class="collapse" id="collapseExampleP">
-                    <cite><?php echo $randCitatP;?></cite>
-                    <p><?php echo $randAutorP;?></p>
-                </div>                
-                <div class="collapse" id="collapseExampleZ">
-                    <cite><?php echo $randCitatZ;?></cite>
-                    <p><?php echo $randAutorZ;?></p>
+                <div class="carousel-item">
+                    <img src="images/<?php echo $arrayPh[1]?>.jpg" alt="<?php echo $arrayPh[1]?>" >
                 </div>
-                <div class="collapse" id="collapseExampleLj">
-                    <cite><?php echo $randCitatLj;?></cite>
-                    <p><?php echo $randAutorLj;?></p>
+                <div class="carousel-item">
+                    <img src="images/<?php echo $arrayPh[2]?>.jpg" alt="<?php echo $arrayPh[2]?>" >
                 </div>
-                <div class="collapse" id="collapseExampleM">
-                    <cite><?php echo $randCitatM;?></cite>
-                    <p><?php echo $randAutorM;?></p>
-                </div> 
+            </div>
+
+            <!-- Left and right controls -->
+            <a class="carousel-control-prev" href="#demo" data-slide="prev">
+                <span class="carousel-control-prev-icon"></span>
+            </a>
+            <a class="carousel-control-next" href="#demo" data-slide="next">
+                <span class="carousel-control-next-icon"></span>
+            </a>
+        </div>    
+    </header>    
+
+      <!-- nav -->
+    <nav>
+        <div class="container-fluid">
+            <div class="row">
+                <div class="btn navItem col-sm-3 navbar-dark bg-dark citatiNav">
+                    <a class="btn btn-link btn-block collapsed" role="button" data-toggle="collapse" data-target="#posao" aria-expanded="false"
+                        aria-controls="posao">
+                        POSAO
+                    </a>
+                </div>
+                <div class="btn navItem col-sm-3 navbar-dark bg-dark citatiNav">
+                    <a class="btn btn-link btn-block collapsed" role="button" data-toggle="collapse" data-target="#zdravlje" aria-expanded="false"
+                        aria-controls="zdravlje">
+                        ZDRAVLJE
+                    </a>
+                </div>
+                <div class="btn navItem col-sm-3 navbar-dark bg-dark citatiNav">
+                    <a class="btn btn-link btn-block collapsed" role="button" data-toggle="collapse" data-target="#ljubav" aria-expanded="false"
+                        aria-controls="ljubav">
+                        LJUBAV
+                </a>
+                </div>
+                <div class="btn navItem col-sm-3 navbar-dark bg-dark citatiNav">
+                    <a class="btn btn-link btn-block collapsed" role="button" data-toggle="collapse" data-target="#motivacija" aria-expanded="false"
+                        aria-controls="motivacija">
+                        MOTIVACIJA
+                    </a>
+                </div>
             </div>
         </div>
-    </div>
-<!-- nov section  -->
- 
+    </nav>
 
+    <!-- main -->
 
+    <section>
+        <div class="container-fluid">
+            
+            <div id="accordion">
+                <div class="card">
+                    <div id="random" class="collapse show" aria-labelledby="random" data-parent="#accordion">
+                        <div class="card-body">
+                            <cite><?php echo $randCitat;?></cite>
+                            <p><?php echo $randAutor;?></p>
+                        </div>
+                    </div>
+                </div>
+                <div class="card">
+                    <div id="posao" class="collapse" aria-labelledby="posao" data-parent="#accordion">
+                        <div class="card-body">
+                            <cite><?php echo $randCitatP;?></cite>
+                            <p><?php echo $randAutorP;?></p>
+                        </div>
+                    </div>
+                </div>
+                <div class="card">
+                    <div id="zdravlje" class="collapse" aria-labelledby="zdravlje" data-parent="#accordion">
+                        <div class="card-body">
+                            <cite><?php echo $randCitatZ;?></cite>
+                            <p><?php echo $randAutorZ;?></p>
+                        </div>
+                    </div>
+                </div>
+                <div class="card">
+                    <div id="ljubav" class="collapse" aria-labelledby="ljubav" data-parent="#accordion">
+                        <div class="card-body">
+                            <cite><?php echo $randCitatLj;?></cite>
+                            <p><?php echo $randAutorLj;?></p>
+                        </div>
+                    </div>
+                </div>
+
+                <div class="card">
+                    <div id="motivacija" class="collapse" aria-labelledby="mnotivacija" data-parent="#accordion">
+                        <div class="card-body">
+                            <cite><?php echo $randCitatM;?></cite>
+                            <p><?php echo $randAutorM;?></p>
+                        </div>
+                    </div>
+                </div>
+            </div>            
+        </div>
+    </section>
 
     <!-- footer -->
-    <footer>
+    <footer class="fixed-bottom">
         <div class="container-fluid">
             <div class="row">                
-                <div class="col-6">
+                <div class="col-6" id="time">
                     <p><?php echo date('H:i A'); ?></p>
                 </div>
-                <div class="col-6">
-                    <p><?php echo date('l jS \of F Y'); ?></p>
+                <div class="col-6" id="date">
+                    <p><?php echo date('j\. F Y\.'); ?></p>                                      
                 </div>                
+            </div>
+            <div class="row" >
+                <div class="col-12" id=social>
+                    If you liked our website, follow us for more:                    
+                    <a href="https://github.com/tomicevamarijana" target=_blank><i class="fa fa-github" aria-hidden="true"></i></a>
+                    <a href="https://www.linkedin.com/in/marijana-tomi%C4%87-754391242/"><i class="fa fa-linkedin" aria-hidden="true"> </i></a>
+                    <a href="https://www.facebook.com/profile.php?id=100001498511315" target=_blank><i class="fa fa-facebook-official" aria-hidden="true"> </i></a>
+                </div>
             </div>
         </div>
     </footer>
     
-<script src="https://cdn.jsdelivr.net/npm/bootstrap@5.2.3/dist/js/bootstrap.bundle.min.js" integrity="sha384-kenU1KFdBIe4zVF0s0G1M5b4hcpxyD9F7jL+jjXkk+Q2h455rYXK/7HAuoJl+0I4" crossorigin="anonymous"></script>
+    <script src="https://cdn.jsdelivr.net/npm/jquery@3.5.1/dist/jquery.slim.min.js" integrity="sha384-DfXdz2htPH0lsSSs5nCTpuj/zy4C+OGpamoFVy38MVBnE+IbbVYUew+OrCXaRkfj" crossorigin="anonymous"></script>
+    <script src="https://cdn.jsdelivr.net/npm/bootstrap@4.6.2/dist/js/bootstrap.bundle.min.js" integrity="sha384-Fy6S3B9q64WdZWQUiU+q4/2Lc9npb8tCaSX9FK7E8HnRr0Jz8D6OP9dO5Vg3Q9ct" crossorigin="anonymous"></script>
 </body>
 </html>
